a301582b7fa79b922201880775fcfa0b45cbd1e8
Carlos Molina <carlosamolina19@gmail.com>
2025-03-17 18:12:23 -0600

    feature: add project

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Carlos Molina Sáenz on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
